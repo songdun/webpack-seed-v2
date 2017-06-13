9a8d212598bb114d6f3df9ac49e8bec47d9f3656
songdun <songdun@iciyun.com>
2017-06-13 15:25:31 +0800

    修改 清除更新model后再次发送id为空的请求

--- example-admin-2-single-page/src/common/model/itemModel/fetch.js
+++ example-admin-2-single-page/src/common/model/itemModel/fetch.js
@@ -1,12 +1,28 @@
 import ajax from "srcDir/common/ajax/index";
 
 const fetch = function ({ url, method, params, success }) {
-  return ajax({
-    method,
-    url,
-    params,
-    success,
-  });
+  // console.log(params);
+  let result = null;
+  if (params) {
+    result = ajax({
+      method,
+      url,
+      params,
+      success,
+    });
+  } else {
+    result = Promise.resolve({
+      type: "dataUpdate",
+      value: JSON.stringify({
+        msg: "",
+        obj: {},
+        success: true,
+        type: 0,
+        fieldErrors: {}
+      }),
+    });
+  }
+  return result;
 };
 
 export { fetch as default };
